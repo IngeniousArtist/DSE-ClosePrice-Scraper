beccbd7ee48da82298fd6f5ab2244ffd92728199
Mohammad Muhtasim Shahriyer <ingeniousartist@gmail.com>
2019-12-14 23:49:14 +0600

    changed parsing to beautiful soup

--- scrape_DSE.py
+++ scrape_DSE.py
@@ -1,6 +1,11 @@
+#DSE BD Closing Price Scraper
+#by
+#http://github.com/IngeniousArtist
+
 import csv
 from datetime import datetime
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 start = datetime.now()
 
@@ -17,18 +22,32 @@ driver.execute_script("arguments[0].min = '2015-01-01'", from_element)
 to_element =driver.find_element_by_xpath(to_date)
 driver.execute_script("arguments[0].min = '2015-01-01'", to_element)
 
+#10957 Rows
 driver.find_element_by_xpath(from_date).send_keys("01/01/2015")
 driver.find_element_by_xpath(to_date).send_keys("30/06/2015")
 driver.find_element_by_xpath(btn).click()
 
-table_path = "/html/body/table[2]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/table"
-table = driver.find_element_by_xpath(table_path)
-with open('/Users/shahriyer/Desktop/code/data_2015_1.csv', 'w', newline='') as csvfile:
-    wr = csv.writer(csvfile)
-    for row in table.find_elements_by_tag_name('tr'):
-        wr.writerow([d.text for d in row.find_elements_by_tag_name('td')])
+content = driver.page_source
+
+soup = BeautifulSoup(content, 'lxml')
 
 driver.close()
+
+stock_table = soup.find("table", attrs={"border":"0", "cellpadding":"3", "width":"100%","bgcolor":"#808000", "cellspacing":"1"})
+stock_table_data = stock_table.tbody.find_all("tr")  # contains whatever rows
+
+print("Number of Rows in Dataset:")
+print(len(stock_table_data))
+
+data = [[] for x in range(len(stock_table_data)-1)]
+
+for i in range(len(stock_table_data)-1):
+    for td in stock_table_data[i].find_all("td"):
+        data[i].append(td.font.text)
+
+with open("/Users/shahriyer/Desktop/code/Data_2015_1.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(data)
 
 finish = datetime.now() - start
 
